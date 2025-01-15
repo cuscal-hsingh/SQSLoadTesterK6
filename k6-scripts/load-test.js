fefe0fccdcf23714b32f5ce59c15d4d3feb2a36e
hsingh <hsingh@cuscal.com.au>
2025-01-16 10:39:07 +1100

    Commit 1

---
+++ k6-scripts/load-test.js
@@ -0,0 +1,20 @@
+import { AWSConfig, SQSClient } from 'https://jslib.k6.io/aws/0.12.3/sqs.js';
+
+const awsConfig = new AWSConfig({
+    region: __ENV.AWS_REGION,
+    accessKeyId: __ENV.AWS_ACCESS_KEY_ID,
+    secretAccessKey: __ENV.AWS_SECRET_ACCESS_KEY,
+    sessionToken: __ENV.AWS_SESSION_TOKEN,
+});
+
+const sqs = new SQSClient(awsConfig);
+const queueUrl = 'https://sqs.ap-southeast-2.amazonaws.com/990522850489/SQSLoadTesterK6';
+
+export default async function () {
+    await sqs.sendMessage(queueUrl, 'test message', {
+        messageAttributes: {
+            'Attribute1': { type: 'String', value: 'Value1' },
+            'Attribute2': { type: 'Number', value: '123' },
+        },
+    });
+}
