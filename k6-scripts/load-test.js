401cde2702e2923404facd1ae3c2527b5b4787e2
hsingh <hsingh@cuscal.com.au>
2025-01-20 11:57:45 +1100

    commit 23

--- k6-scripts/load-test.js
+++ k6-scripts/load-test.js
@@ -1,5 +1,12 @@
 import { AWSConfig, SQSClient } from 'https://jslib.k6.io/aws/0.12.3/sqs.js';
 
+
+// Debugging: Print environment variables
+console.log(`AWS_REGION: ${__ENV.AWS_REGION}`);
+console.log(`AWS_ACCESS_KEY_ID: ${__ENV.AWS_ACCESS_KEY_ID}`);
+console.log(`AWS_SECRET_ACCESS_KEY: ${__ENV.AWS_SECRET_ACCESS_KEY}`);
+console.log(`AWS_SESSION_TOKEN: ${__ENV.AWS_SESSION_TOKEN}`);
+
 const awsConfig = new AWSConfig({
     region: __ENV.AWS_REGION,
     accessKeyId: __ENV.AWS_ACCESS_KEY_ID,
